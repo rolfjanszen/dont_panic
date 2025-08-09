82beaa7b560932ab686d47c3ab9e989b6add17f1
rolf <rolfjanszen@hotmail.com>
2025-08-09 12:36:07 +0200

    changes to readme

--- README.m
+++ README.m
@@ -1,1 +1,33 @@
 
+Install dependencies:
+
+sudo pip3 install django djangorestframework yfinance plotly django-cors-headers
+
+Start backend:
+python3 manage.py migrate
+python3 manage.py runserver
+
+Debug in vscode:
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "Python: Django",
+      "type": "python",
+      "request": "launch",
+      "program": "${workspaceFolder}/manage.py",
+      "args": [
+        "runserver",
+        "--noreload"
+      ],
+      "django": true
+    }
+  ]
+}
+
+Setup the frontned
+Frontend uses plotly.js-dist-min for plotting
+In /frontend:
+npm install
+npm install axios
+npm run dev
